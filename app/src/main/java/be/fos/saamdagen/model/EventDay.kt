6bb347df95824cdb88acb66dd72a2d7e7f06d6cd
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-03-26 17:29:51 +0100

    Show dates in Dutch

--- app/src/main/java/be/fos/saamdagen/model/EventDay.kt
+++ app/src/main/java/be/fos/saamdagen/model/EventDay.kt
@@ -3,9 +3,9 @@ package be.fos.saamdagen.model
 import java.text.SimpleDateFormat
 import java.util.*
 
-private const val formatPattern = "d MMMM"
+private const val formatPattern = "EEEE"
 
-val FORMATTER_MONTH_DAY: SimpleDateFormat = SimpleDateFormat(formatPattern)
+val FORMATTER_MONTH_DAY: SimpleDateFormat = SimpleDateFormat(formatPattern,Locale("nl"))
 
 data class EventDay(val start: Date, val end: Date) {
 

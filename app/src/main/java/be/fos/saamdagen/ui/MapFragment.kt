045769222f106f870b1a936222b11b7a1ac6684e
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-08-20 15:06:56 +0200

    Add basic Google Map

--- app/src/main/java/be/fos/saamdagen/ui/MapFragment.kt
+++ app/src/main/java/be/fos/saamdagen/ui/MapFragment.kt
@@ -6,19 +6,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import be.fos.saamdagen.R
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.fragment_map.*
+import kotlinx.android.synthetic.main.fragment_map.view.*
 
 
 class MapFragment : Fragment() {
 
+    private lateinit var mapView: MapView
 
-
+    private val position = LatLng(51.151006,3.881024)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        var view =
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+         inflater.inflate(R.layout.fragment_map, container, false)
+
+        this.mapView = view.map
+        with(this.mapView) {
+            onCreate(null)
+            getMapAsync {
+                MapsInitializer.initialize(context)
+                setMapLocation(it)
+            }
+        }
+
+        return view;
+    }
+
+    private fun setMapLocation(map: GoogleMap) {
+        with(map) {
+            moveCamera(CameraUpdateFactory.newLatLngZoom(position,17f))
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
 }

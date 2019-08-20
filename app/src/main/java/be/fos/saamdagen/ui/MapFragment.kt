ab80b5fe70f354245e1a8195eb1141e02d0a3486
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-08-20 15:36:41 +0200

    Show user location on map

--- app/src/main/java/be/fos/saamdagen/ui/MapFragment.kt
+++ app/src/main/java/be/fos/saamdagen/ui/MapFragment.kt
@@ -1,20 +1,29 @@
 package be.fos.saamdagen.ui
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import be.fos.saamdagen.R
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.fragment_map.*
 import kotlinx.android.synthetic.main.fragment_map.view.*
+import android.content.DialogInterface
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 
 
 class MapFragment : Fragment() {
 
     private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
 
     private val position = LatLng(51.151006,3.881024)
 
@@ -28,13 +37,24 @@ class MapFragment : Fragment() {
          inflater.inflate(R.layout.fragment_map, container, false)
 
         this.mapView = view.map
+        checkLocationPermission()
         with(this.mapView) {
             onCreate(null)
             getMapAsync {
+                googleMap = it
                 MapsInitializer.initialize(context)
                 setMapLocation(it)
+
+                if (ContextCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                    googleMap.isMyLocationEnabled = true
+                }
             }
         }
+
+
 
         return view;
     }
@@ -44,9 +64,93 @@ class MapFragment : Fragment() {
             moveCamera(CameraUpdateFactory.newLatLngZoom(position,17f))
         }
     }
+
+
+    val MY_PERMISSIONS_REQUEST_LOCATION = 99
+
+    fun checkLocationPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity as Activity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Locatie")
+                    .setMessage("Message")
+                    .setPositiveButton("OK", DialogInterface.OnClickListener { dialogInterface, i ->
+                        //Prompt the user once explanation has been shown
+                        ActivityCompat.requestPermissions(
+                            activity as Activity,
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            MY_PERMISSIONS_REQUEST_LOCATION
+                        )
+                    })
+                    .create()
+                    .show()
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                    activity as Activity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    MY_PERMISSIONS_REQUEST_LOCATION
+                )
+            }
+            return false
+        } else {
+            return true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+
+                        Log.d("FRAGMENT_MAP","PERMISSION")
+                    googleMap.isMyLocationEnabled = true
+                        //Request location updates:
+                       // locationManager.requestLocationUpdates(provider, 400, 1, this)
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return
+            }
+        }
+    }
     override fun onResume() {
         super.onResume()
         mapView.onResume()
+
     }
 
     override fun onPause() {

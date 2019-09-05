708f643cba95b4d420a13b0a0d70130a969fbeb8
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-09-05 16:29:05 +0200

    Add open source licenses dialog

--- app/src/main/java/be/fos/saamdagen/ui/settings/SettingsFragment.kt
+++ app/src/main/java/be/fos/saamdagen/ui/settings/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package be.fos.saamdagen.ui.settings
 
+import android.app.AlertDialog
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.databinding.BindingAdapter
 
 import be.fos.saamdagen.R
 import be.fos.saamdagen.databinding.FragmentSettingBinding
@@ -29,4 +32,17 @@ class SettingsFragment : Fragment() {
         return binding.root
     }
 
+}
+
+@BindingAdapter(value = ["dialogTitle", "fileLink"], requireAll = true)
+fun createDialogForFile(button: View, dialogTitle: String, fileLink: String) {
+    val context = button.context
+    button.setOnClickListener {
+        val webView = WebView(context).apply { loadUrl(fileLink) }
+        AlertDialog.Builder(context)
+            .setTitle(dialogTitle)
+            .setView(webView)
+            .create()
+            .show()
+    }
 }

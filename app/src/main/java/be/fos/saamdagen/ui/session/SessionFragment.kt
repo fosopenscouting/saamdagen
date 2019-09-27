ef473e4379455c5c438571da9c3753bf7dd13c4c
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-09-27 12:14:08 +0200

    Bump version

--- app/src/main/java/be/fos/saamdagen/ui/session/SessionFragment.kt
+++ app/src/main/java/be/fos/saamdagen/ui/session/SessionFragment.kt
@@ -6,29 +6,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import be.fos.saamdagen.R
 import be.fos.saamdagen.databinding.FragmentSessionBinding
 import be.fos.saamdagen.model.SessionType
 import com.microsoft.appcenter.analytics.Analytics
 import kotlinx.android.synthetic.main.fragment_session.*
+import androidx.lifecycle.Observer
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.dialog_search.view.*
 
-class  SessionFragment : Fragment() {
+
+class SessionFragment : Fragment() {
 
     //UI
     private lateinit var binding: FragmentSessionBinding
 
     private lateinit var viewModel: SessionViewModel
 
+    private lateinit var adapter: SessionParticipantAdapter
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSessionBinding.inflate(inflater,container,false).apply {
+        viewModel = ViewModelProviders.of(this).get(SessionViewModel::class.java)
+        binding = FragmentSessionBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = this@SessionFragment
         }
 
         Analytics.trackEvent("Activiteiten geopend")
+
+         adapter = SessionParticipantAdapter()
+
+
+        binding.searchFab.setOnClickListener {
+            showSearchDialog()
+
+        }
+
+
+
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        viewModel.participantSessions.observe(this, Observer {
+
+
+        })
 
 
         return binding.root
@@ -42,11 +70,14 @@ class  SessionFragment : Fragment() {
         viewpager.adapter = ActivityAdapter(childFragmentManager)
     }
 
-    inner class ActivityAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+    inner class ActivityAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
         //TODO: opzoeken of dit wel ok is. Seems fishy.
-        private val types = arrayOf(SessionType("Activiteiten","activiteit"), SessionType("Workshops","workshop"))
-        override fun getItem(position: Int): Fragment = SessionPageFragment.newInstance(types[position])
+        private val types =
+            arrayOf(SessionType("Activiteiten", "activiteit"), SessionType("Workshops", "workshop"))
+
+        override fun getItem(position: Int): Fragment =
+            SessionPageFragment.newInstance(types[position])
 
         override fun getCount(): Int {
             return types.size
@@ -57,5 +88,33 @@ class  SessionFragment : Fragment() {
         }
 
     }
+
+
+    private fun showSearchDialog() {
+        val builder = AlertDialog.Builder(context!!)
+
+        builder.setTitle("Activiteiten zoeken")
+
+        val inflatedView = LayoutInflater.from(context!!)
+            .inflate(R.layout.dialog_search, view as ViewGroup, false)
+
+
+        builder.setView(inflatedView)
+
+        // Set up the buttons
+        builder.setPositiveButton(
+            android.R.string.ok
+        ) { dialog, which ->
+            dialog.dismiss()
+           viewModel.findSessionsFor(inflatedView.input.text.toString())
+        }
+        builder.setNegativeButton(
+            android.R.string.cancel
+        ) { dialog, which -> dialog.cancel() }
+
+        builder.show()
+    }
+
+
 
 }

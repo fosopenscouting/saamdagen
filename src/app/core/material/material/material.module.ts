e41fe1b4cf67578b6f4f749511c22f3761e4faa5
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-03-25 20:16:00 +0100

    Add Material module

---
+++ src/app/core/material/material/material.module.ts
@@ -0,0 +1,49 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatRippleModule,
+    MatBottomSheetModule,
+    MatSnackBarModule,
+    MatMenuModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatRippleModule,
+    MatBottomSheetModule,
+    MatSnackBarModule,
+    MatMenuModule
+  ]
+})
+export class MaterialModule {}

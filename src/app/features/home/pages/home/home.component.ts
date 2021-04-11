ad8a4f235bbb4bfae9a32c6a940a0205d1b93913
Thomas de Wulf <thomas.de.wulf@telenet.be>
2021-04-11 18:25:55 +0200

    Uodate title for home component

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppTitleService } from 'src/app/core/title/app-title.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: AppTitleService) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('Saamdagen')
   }
 
 }

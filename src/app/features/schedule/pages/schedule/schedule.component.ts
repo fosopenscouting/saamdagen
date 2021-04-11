5427f27f1d4556cfd96d50105190c8575454932f
Thomas de Wulf <thomas.de.wulf@telenet.be>
2021-04-11 18:46:41 +0200

    Update title

--- src/app/features/schedule/pages/schedule/schedule.component.ts
+++ src/app/features/schedule/pages/schedule/schedule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppTitleService } from 'src/app/core/title/app-title.service';
 
 @Component({
   selector: 'app-schedule',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: AppTitleService) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('Programma')
   }
 
 }

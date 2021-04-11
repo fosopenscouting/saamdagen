760341b06a5ca8519ffa251e55cf87158c9f0e89
Thomas de Wulf <thomas.de.wulf@telenet.be>
2021-04-11 18:22:37 +0200

    Add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./features/home/home.module').then(mod => mod.HomeModule)
+  },
+  {
     path: 'settings',
     loadChildren: () => import('./features/settings/settings.module').then(mod => mod.SettingsModule)
   },
@@ -16,6 +20,10 @@ const routes: Routes = [
   {
     path: 'map',
     loadChildren: () => import('./features/map/map.module').then(mod => mod.MapModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
   }
 ];
 
---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MaterialModule } from 'src/app/core/material/material.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+]
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class HomeModule { }

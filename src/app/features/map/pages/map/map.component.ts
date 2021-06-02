3bcf0ba931b99c1c7aaa03962828e08ddf2444d6
Thomas de Wulf <thomas.de.wulf@telenet.be>
2021-06-02 20:53:32 +0200

    Center map properly

--- src/app/features/map/pages/map/map.component.ts
+++ src/app/features/map/pages/map/map.component.ts
@@ -9,8 +9,8 @@ import { AppTitleService } from 'src/app/core/title/app-title.service';
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
-  zoom = 19;
-  center = { lng: 4.850511, lat: 51.200876 };
+  zoom = 17;
+  center = { lng: 4.849884510040283, lat: 51.20256112288838 };
 
   @ViewChild(GoogleMap, { static: false }) map: GoogleMap
 

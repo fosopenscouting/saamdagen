8940775ba93139ff523fda84ae6f26219036bfe9
Thomas de Wulf <thomas.de.wulf@telenet.be>
2021-07-27 19:26:56 +0200

    Ask for location permission

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { View } from '../components/Themed';
 import { Dimensions, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Region } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const MapScreen: React.FC = () => {
+  const mapRegion: Region = {
+    latitude: 51.200977,
+    longitude: 4.850671,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+  };
+
+  const onMapReady = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        provider="google"
+        region={mapRegion}
+        showsUserLocation={true}
+        minZoomLevel={16}
+        showsMyLocationButton
+        onMapReady={onMapReady}
+      />
     </View>
   );
 };

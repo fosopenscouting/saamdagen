ce01d4ca960c2b328f73ae5cc719702745fdb59b
Thomas de Wulf <thomas.de.wulf@telenet.be>
2021-08-07 10:04:57 +0200

    Extract FAB to its own component

---
+++ components/Map/MapFab.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useState } from 'react';
+import { FAB, Portal, Provider } from 'react-native-paper';
+import Colors from '../../constants/Colors';
+import useColorScheme from '../../hooks/useColorScheme';
+import { MapLayer } from '../../models/MapLayer';
+
+interface MapFabProps {
+  handleLayerSelect: (layer: MapLayer) => void;
+}
+
+const MapFab: React.FC<MapFabProps> = (props: MapFabProps) => {
+  const colorScheme = useColorScheme();
+  const [fabOpen, setFabOpen] = useState(false);
+
+  return (
+    <Provider>
+      <Portal>
+        <FAB.Group
+          visible
+          fabStyle={{ backgroundColor: Colors[colorScheme].tabBackground }}
+          open={fabOpen}
+          icon="layers"
+          actions={[
+            {
+              icon: 'basketball',
+              label: 'Activiteiten',
+              onPress: () => props.handleLayerSelect('activities'),
+            },
+            {
+              icon: 'map-marker-outline',
+              label: 'Normaal',
+              onPress: () => props.handleLayerSelect('normal'),
+            },
+          ]}
+          onStateChange={() => setFabOpen(!fabOpen)}
+        />
+      </Portal>
+    </Provider>
+  );
+};
+
+export default MapFab;
--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -9,20 +9,16 @@ import { useCallback } from 'react';
 import { getMapMarkers } from '../services/DataService';
 import MapDetail from '../components/Map/MapDetail';
 import { PointOfInterest } from '../models/PointOfInterest';
-import { FAB, Portal, Provider } from 'react-native-paper';
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
 import { useEffect } from 'react';
 import { MapLayer } from '../models/MapLayer';
+import MapFab from '../components/Map/MapFab';
 
 const MapScreen: React.FC = () => {
-  const [selectedMarker, setSelectedMarker] = useState<MapMarker>();
+  const [selectedMarker, setSelectedMarker] = useState<MapMarker | null>();
   const [layer, setLayer] = useState<MapLayer>('normal');
   const [markers, setMarkers] = useState<Map<PointOfInterest, MapMarker>>();
-  const [fabOpen, setFabOpen] = useState<boolean>(false);
   const sheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ['25%', '50%'], []);
-  const colorScheme = useColorScheme();
 
   const mapRegion: Region = {
     latitude: 51.200977,
@@ -32,6 +28,7 @@ const MapScreen: React.FC = () => {
   };
 
   useEffect(() => {
+    setSelectedMarker(null);
     const newMarkers: Map<PointOfInterest, MapMarker> = new Map();
     getMapMarkers().forEach((value: MapMarker, key: PointOfInterest) => {
       if (value.layer === layer) {
@@ -42,16 +39,19 @@ const MapScreen: React.FC = () => {
     setMarkers(newMarkers);
   }, [layer]);
 
+  useEffect(() => {
+    sheetRef.current?.snapTo(0);
+  }, [selectedMarker]);
+
   const onMapReady = async () => {
     await Location.requestForegroundPermissionsAsync();
   };
 
   const handleMarkerSelect = useCallback(
     (markerIdentifier: PointOfInterest) => {
+      console.log(markerIdentifier);
       const markerObject = markers?.get(markerIdentifier);
       setSelectedMarker(markerObject);
-
-      sheetRef.current?.snapTo(0);
     },
     [],
   );
@@ -60,8 +60,11 @@ const MapScreen: React.FC = () => {
     sheetRef.current?.close();
   }, []);
 
-  const handleLayerSelect = (layer: MapLayer): void => {
-    setLayer(layer);
+  const handleLayerSelect = (newLayer: MapLayer): void => {
+    if (layer !== newLayer) {
+      sheetRef.current?.close();
+      setLayer(newLayer);
+    }
   };
 
   const renderMarkers = () => {
@@ -111,29 +114,7 @@ const MapScreen: React.FC = () => {
           ) : null}
         </BottomSheet>
       ) : null}
-      <Provider>
-        <Portal>
-          <FAB.Group
-            visible
-            fabStyle={{ backgroundColor: Colors[colorScheme].tabBackground }}
-            open={fabOpen}
-            icon="layers"
-            actions={[
-              {
-                icon: 'map-marker-outline',
-                label: 'Normaal',
-                onPress: () => handleLayerSelect('normal'),
-              },
-              {
-                icon: 'basketball',
-                label: 'Activiteiten',
-                onPress: () => handleLayerSelect('activities'),
-              },
-            ]}
-            onStateChange={() => setFabOpen(!fabOpen)}
-          />
-        </Portal>
-      </Provider>
+      <MapFab handleLayerSelect={handleLayerSelect} />
     </View>
   );
 };

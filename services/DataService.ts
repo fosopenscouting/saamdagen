de9179198bdc826b1b141f4bb36107d571a9e9f7
Thomas de Wulf <thomas.de.wulf@telenet.be>
2021-09-14 17:12:06 +0200

    More data

--- services/DataService.ts
+++ services/DataService.ts
@@ -10,7 +10,7 @@ const markers = new Map<PointOfInterest | string, MapMarker>([
     'Infopunt',
     {
       title: 'Infopunt',
-      description: `Aan het infopunt kan je terecht voor … info! Hier koop je ook jetons en bekers. Oordoppen of condooms nodig? Zit je met een vraag of zin in een babbel? Het infopunt is the place to be!
+      description: `Aan het infopunt kan je terecht voor … info! Hier koop je ook jetons en bekers. Oordoppen of condooms nodig? Zit je met een vraag of zin in een babbel? Het infopunt is the place to be! Ook voor EHBO kan je hier terecht.
 
 We werken met herbruikbare bekers. Voor €2 koop je een beker met bekerjeton aan. Het hele weekend maak je gebruik van je beker. Als je aan de bar een drankje koopt, geef je je beker af. Je krijgt een gevulde beker terug. Die beker gebruik je dan opnieuw als je een drankje haalt. Op het einde van het weekend kan je bij de uitcheck de €2 terugkrijgen als je een beker en een bekerjeton afgeeft. Draag dus goed zorg voor je bekerjeton.
 
@@ -114,6 +114,10 @@ Ook de overschot van je jetons kan je op het einde van het weekend inruilen voor
         longitude: 4.850218,
       },
       icon: require('../assets/images/map-icons/icon-douche.png'),
+      description: `Hier vind je:
+      - Toiletten
+      - Douches
+      - Kraantjeswater`,
     },
   ],
   [
@@ -126,6 +130,9 @@ Ook de overschot van je jetons kan je op het einde van het weekend inruilen voor
         longitude: 4.849293,
       },
       icon: require('../assets/images/map-icons/icon-douche.png'),
+      description: `Hier vind je:
+      - Toiletten
+      - Kraantjeswater`,
     },
   ],
   [

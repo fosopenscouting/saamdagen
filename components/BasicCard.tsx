591b0685a03067c44510b132d5d846c5398124a9
Pieter Vantorre <pietervantorre@gmail.com>
2022-06-13 19:05:41 +0200

    Test markdown

--- components/BasicCard.tsx
+++ components/BasicCard.tsx
@@ -5,6 +5,7 @@ import ContentCard, { ThemeIdentifier, themes } from './ContentCard';
 import ContentCardTitle from './ContentCardTitle';
 import { Text } from './Themed';
 import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
+import Markdown from 'react-native-easy-markdown';
 
 const getTextColor = (
   colorScheme: 'light' | 'dark',
@@ -38,6 +39,20 @@ const BasicCard: React.FC<Props> = (props: Props) => {
       mode={props.mode}
       palette={props.palette}
     >
+      <Markdown>
+        {
+          '# Why is markdown cool?\n\n' +
+
+          '* because it lets us do simple formatting **easily** \n' +
+          '* _without_ the need for complex CMS data structures \n' +
+          '* and you can outsource ~~your~~ work to the content creators! \n\n' +
+  
+          '> This is a blockquote \n\n' +
+  
+          '![We can add images!](http://placehold.it/300x300) \n' +
+          '[Or link to places](http://foobar.com) \n'
+        }
+      </Markdown>
       <Text style={[{ color: textColor }, styles.title]}>{props.title}</Text>
       <Text style={[{ color: textColor }]}>{props.content}</Text>
     </ContentCard>

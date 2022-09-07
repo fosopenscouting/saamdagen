3a7b0d93c5f03dcdc2ba1546fceae36a79136550
Thomas de Wulf <thomas.de.wulf@telenet.be>
2022-09-07 13:55:33 +0200

    Testing

--- app.config.ts
+++ app.config.ts
@@ -3,5 +3,9 @@ import 'dotenv/config';
 
 export default ({ config }: ConfigContext): Partial<ExpoConfig> => ({
   ...config,
-  version: process.env.SEMVER,
+  extra: {
+    eas: {
+      projectId: '7830594e-890b-4c9e-89cf-b91bf1926f72',
+    },
+  },
 });

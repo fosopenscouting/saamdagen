86cfdc8c117af1aec325f07233708fbe20ad6477
Thomas de Wulf <thomas.de.wulf@telenet.be>
2022-09-15 13:53:54 +0200

    Eas update

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,12 @@ import 'dotenv/config';
 
 export default ({ config }: ConfigContext): Partial<ExpoConfig> => ({
   ...config,
+  runtimeVersion: {
+    policy: 'sdkVersion',
+  },
+  updates: {
+    url: 'https://u.expo.dev/7830594e-890b-4c9e-89cf-b91bf1926f72'
+  },
   extra: {
     eas: {
       projectId: '7830594e-890b-4c9e-89cf-b91bf1926f72',

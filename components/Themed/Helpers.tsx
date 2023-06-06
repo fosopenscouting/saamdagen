cec88da5ca9530b7e22ca3953dd21720f30de7df
Pieter Vantorre <pietervantorre@gmail.com>
2023-06-06 21:12:59 +0200

    Lint

--- components/Themed/Helpers.tsx
+++ components/Themed/Helpers.tsx
@@ -3,22 +3,22 @@ import { Text as DefaultText } from 'react-native';
 import Colors from '../../constants/Colors';
 
 export const useThemeColor = (
-    props: { light?: string; dark?: string },
-    colorName: keyof typeof Colors.light & keyof typeof Colors.dark,
-  ) => {
-    const theme = useColorScheme();
-    const colorFromProps = props[theme];
-  
-    if (colorFromProps) {
-      return colorFromProps;
-    } else {
-      return Colors[theme][colorName];
-    }
-  };
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark,
+) => {
+  const theme = useColorScheme();
+  const colorFromProps = props[theme];
+
+  if (colorFromProps) {
+    return colorFromProps;
+  } else {
+    return Colors[theme][colorName];
+  }
+};
 
 export type TextProps = ThemeProps & DefaultText['props'];
 
 export type ThemeProps = {
-    lightColor?: string;
-    darkColor?: string;
-  };
+  lightColor?: string;
+  darkColor?: string;
+};

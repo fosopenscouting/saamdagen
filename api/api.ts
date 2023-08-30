1a080ff11734a60b43fbf4857e1ff0ede2f7a2c1
Thomas de Wulf <thomas.de.wulf@telenet.be>
2023-08-30 21:13:23 +0200

    Correct env vars

--- api/api.ts
+++ api/api.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 export const getContentIndex = async (): Promise<string[]> => {
-
+  console.log(process.env.EXPO_PUBLIC_CONTENT_ROOT!);
   const text = await (
     await fetch(
       'https://fosopenscouting.github.io/Saamdagen-App-inhoud/content.txt',
@@ -11,17 +11,17 @@ export const getContentIndex = async (): Promise<string[]> => {
   array = array
     .filter((x) => x !== 'README.md')
     .filter((x) => x !== '')
-    .filter((x) => x.startsWith(process.env.PUBLIC_CONTENT_ROOT!));
-  return array.map((x) => x.split(`${process.env.PUBLIC_CONTENT_ROOT!}/`)[1]);
+    .filter((x) => x.startsWith(process.env.EXPO_PUBLIC_CONTENT_ROOT!));
+  return array.map(
+    (x) => x.split(`${process.env.EXPO_PUBLIC_CONTENT_ROOT!}/`)[1],
+  );
 };
 
 export const getMarkdown = async (path: string): Promise<string> => {
-  const text = await (
-    await fetch(
-      `https://fosopenscouting.github.io/Saamdagen-App-inhoud/${Cprocess.env.PUBLIC_CONTENT_ROOT!}/${path}`,
-      { cache: 'no-store' },
-    )
-  ).text();
-
-  return text;
+  const text = await fetch(
+    `https://fosopenscouting.github.io/Saamdagen-App-inhoud/${process.env
+      .EXPO_PUBLIC_CONTENT_ROOT!}/${path}`,
+    { cache: 'no-store' },
+  );
+  return await text.text();
 };

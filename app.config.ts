91d1ba1cb876863c829e9b33e87acda6b9aa843c
Tijl Schipper <tijl.schipper@gmail.com>
2024-09-08 15:58:09 +0200

    new spash screen
    Signed-off-by: Tijl Schipper <tijl.schipper@gmail.com>

--- app.config.ts
+++ app.config.ts
@@ -2,27 +2,28 @@ import { ExpoConfig, ConfigContext } from '@expo/config';
 import 'dotenv/config';
 
 export default ({ config }: ConfigContext): Partial<ExpoConfig> => ({
-  ...config,
-  runtimeVersion: {
-    policy: 'sdkVersion',
-  },
-  updates: {
-    url: 'https://u.expo.dev/7830594e-890b-4c9e-89cf-b91bf1926f72',
-  },
-  extra: {
-    eas: {
-      projectId: '7830594e-890b-4c9e-89cf-b91bf1926f72',
-    },
-  },
-  plugins: [
-    [
-      '@sentry/react-native/expo',
-      {
-        organization: process.env.SENTRY_ORG,
-        project: process.env.SENTRY_PROJECT,
-      },
-    ],
-    'expo-asset',
-    'expo-font',
-  ],
+	...config,
+	runtimeVersion: {
+		policy: 'sdkVersion',
+	},
+	updates: {
+		url: 'https://u.expo.dev/3f3b2355-b2f7-45aa-889f-541b41c35209',
+	},
+	extra: {
+		eas: {
+			projectId: '3f3b2355-b2f7-45aa-889f-541b41c35209',
+		},
+	},
+	plugins: [
+		[
+			'@sentry/react-native/expo',
+			{
+				organization: process.env.SENTRY_ORG,
+				project: process.env.SENTRY_PROJECT,
+			},
+		],
+		'expo-asset',
+		'expo-font',
+		'expo-router',
+	],
 });

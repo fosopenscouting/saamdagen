b1e810b0fd3da946d56ff7fe334d63cdd9aa88d7
Tijl Schipper <tijl.schipper@gmail.com>
2024-09-09 15:48:41 +0200

    misc
    Signed-off-by: Tijl Schipper <tijl.schipper@gmail.com>

--- api/api.ts
+++ api/api.ts
@@ -1,27 +1,30 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+const CONTENT_ROOT = process.env.EXPO_PUBLIC_CONTENT_ROOT! ?? 'Staging'
+
 export const getContentIndex = async (): Promise<string[]> => {
-  console.log(process.env.EXPO_PUBLIC_CONTENT_ROOT!);
-  const text = await (
-    await fetch(
-      'https://fosopenscouting.github.io/Saamdagen-App-inhoud/content.txt',
-      { cache: 'no-store' },
-    )
-  ).text();
-  let array = text.split('\n');
-  array = array
-    .filter((x) => x !== 'README.md')
-    .filter((x) => x !== '')
-    .filter((x) => x.startsWith(process.env.EXPO_PUBLIC_CONTENT_ROOT!));
-  return array.map(
-    (x) => x.split(`${process.env.EXPO_PUBLIC_CONTENT_ROOT!}/`)[1],
-  );
+	console.log(`CONTENT_DIR: ${CONTENT_ROOT}`);
+	const text = await (
+		await fetch(
+		'https://fosopenscouting.github.io/Saamdagen-App-inhoud/content.txt',
+		{ cache: 'no-store' },
+		)
+	).text();
+	let array = text.split('\n');
+
+	array = array
+		.filter((x) => x !== 'README.md')
+		.filter((x) => x !== '')
+		.filter((x) => x.startsWith(CONTENT_ROOT));
+	return array.map(
+		(x) => x.split(`${CONTENT_ROOT}/`)[1],
+	);
 };
 
 export const getMarkdown = async (path: string): Promise<string> => {
-  const text = await fetch(
-    `https://fosopenscouting.github.io/Saamdagen-App-inhoud/${process.env
-      .EXPO_PUBLIC_CONTENT_ROOT!}/${path}`,
-    { cache: 'no-store' },
-  );
-  return await text.text();
+	const text = await fetch(
+		`https://fosopenscouting.github.io/Saamdagen-App-inhoud/${CONTENT_ROOT}/${path}`,
+		{ cache: 'no-store' },
+	);
+	return await text.text();
 };

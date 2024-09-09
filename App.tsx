3cc5f211b6148d21ded09d2d14e16158d08a8904
Tijl Schipper <tijl.schipper@gmail.com>
2024-09-09 15:48:41 +0200

    misc
    Signed-off-by: Tijl Schipper <tijl.schipper@gmail.com>

--- App.tsx
+++ App.tsx
@@ -6,11 +6,11 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import {
-  useFonts,
-  Quicksand_300Light,
-  Quicksand_400Regular,
-  Quicksand_600SemiBold,
-  Quicksand_500Medium,
+    useFonts,
+    Quicksand_300Light,
+    Quicksand_400Regular,
+    Quicksand_600SemiBold,
+    Quicksand_500Medium,
 } from '@expo-google-fonts/quicksand';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import * as Sentry from '@sentry/react-native';
@@ -22,126 +22,132 @@ import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
 Sentry.init({
-  dsn: 'https://b852c07fe977471c96a3fb2dc1e10a49@o446803.ingest.sentry.io/4505356514426880',
-  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+    dsn: 'https://e6974b22a166bbbaea71b4996019d0e5@o4507883031232512.ingest.de.sentry.io/4507885339869264',
+    debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 });
 
 Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
 });
 
 function handleRegistrationError(errorMessage: string) {
-  //alert(errorMessage);
-  throw new Error(errorMessage);
+    //alert(errorMessage);
+    throw new Error(errorMessage);
 }
 
 async function registerForPushNotificationsAsync() {
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
 
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
+    if (Device.isDevice) {
+        const { status: existingStatus } =
+            await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            handleRegistrationError(
+                'Permission not granted to get push token for push notification!',
+            );
+            return;
+        }
+        const projectId =
+            Constants?.expoConfig?.extra?.eas?.projectId ??
+            Constants?.easConfig?.projectId;
+        if (!projectId) {
+            handleRegistrationError('Project ID not found');
+        }
+        try {
+            const pushTokenString = (
+                await Notifications.getExpoPushTokenAsync({
+                    projectId,
+                })
+            ).data;
+            console.log(pushTokenString);
+            return pushTokenString;
+        } catch (e: unknown) {
+            handleRegistrationError(`${e}`);
+        }
+    } else {
+        handleRegistrationError(
+            'Must use physical device for push notifications',
+        );
     }
-    if (finalStatus !== 'granted') {
-      handleRegistrationError(
-        'Permission not granted to get push token for push notification!',
-      );
-      return;
-    }
-    const projectId =
-      Constants?.expoConfig?.extra?.eas?.projectId ??
-      Constants?.easConfig?.projectId;
-    if (!projectId) {
-      handleRegistrationError('Project ID not found');
-    }
-    try {
-      const pushTokenString = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId,
-        })
-      ).data;
-      console.log(pushTokenString);
-      return pushTokenString;
-    } catch (e: unknown) {
-      handleRegistrationError(`${e}`);
-    }
-  } else {
-    handleRegistrationError('Must use physical device for push notifications');
-  }
 }
 
 function App(): React.ReactElement | null {
-  const isLoadingComplete = useCachedResources();
-  const [fontsLoaded] = useFonts({
-    Quicksand_300Light,
-    Quicksand_400Regular,
-    Quicksand_600SemiBold,
-    Quicksand_500Medium,
-  });
-  const colorScheme = useColorScheme();
+    const isLoadingComplete = useCachedResources();
+    const [fontsLoaded] = useFonts({
+        Quicksand_300Light,
+        Quicksand_400Regular,
+        Quicksand_600SemiBold,
+        Quicksand_500Medium,
+    });
+    const colorScheme = useColorScheme();
 
-  //Notificaties
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState<
-    Notifications.Notification | undefined
-  >(undefined);
-  const notificationListener = useRef<Notifications.Subscription>();
-  const responseListener = useRef<Notifications.Subscription>();
+    //Notificaties
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState<
+        Notifications.Notification | undefined
+    >(undefined);
+    const notificationListener = useRef<Notifications.Subscription>();
+    const responseListener = useRef<Notifications.Subscription>();
 
-  useEffect(() => {
-    registerForPushNotificationsAsync()
-      .then((token) => setExpoPushToken(token ?? ''))
-      .catch((error: any) => setExpoPushToken(`${error}`));
+    useEffect(() => {
+        registerForPushNotificationsAsync()
+            .then((token) => setExpoPushToken(token ?? ''))
+            .catch((error: any) => setExpoPushToken(`${error}`));
 
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-      });
+        notificationListener.current =
+            Notifications.addNotificationReceivedListener((notification) => {
+                setNotification(notification);
+            });
 
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
+        responseListener.current =
+            Notifications.addNotificationResponseReceivedListener(
+                (response) => {
+                    console.log(response);
+                },
+            );
 
-    return () => {
-      notificationListener.current &&
-        Notifications.removeNotificationSubscription(
-          notificationListener.current,
+        return () => {
+            notificationListener.current &&
+                Notifications.removeNotificationSubscription(
+                    notificationListener.current,
+                );
+            responseListener.current &&
+                Notifications.removeNotificationSubscription(
+                    responseListener.current,
+                );
+        };
+    }, []);
+
+    if (!isLoadingComplete || !fontsLoaded) {
+        return null;
+    } else {
+        return (
+            <RootSiblingParent>
+                <SafeAreaProvider>
+                    <DataContextProvider>
+                        <Navigation colorScheme={colorScheme} />
+                        <StatusBar style="light" />
+                    </DataContextProvider>
+                </SafeAreaProvider>
+            </RootSiblingParent>
         );
-      responseListener.current &&
-        Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
-  if (!isLoadingComplete || !fontsLoaded) {
-    return null;
-  } else {
-    return (
-      <RootSiblingParent>
-        <SafeAreaProvider>
-          <DataContextProvider>
-            <Navigation colorScheme={colorScheme} />
-            <StatusBar style="light" />
-          </DataContextProvider>
-        </SafeAreaProvider>
-      </RootSiblingParent>
-    );
-  }
+    }
 }
 
 export default Sentry.wrap(App);

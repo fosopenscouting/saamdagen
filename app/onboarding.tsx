2b0359c3edb803bb46606e0e8d1d7f872793c7ad
Tijl Schipper <tijl.schipper@gmail.com>
2025-08-25 16:56:38 +0200

    more color fixes
    Signed-off-by: Tijl Schipper <tijl.schipper@gmail.com>

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -32,8 +32,8 @@ const OnboardingScreen = () => {
           title: 'Hey!',
           subtitle: (
             <View>
-              <Text>Welkom bij de Saamdagen app!</Text>
-              <Text>
+              <Text lightColor='#fff'>Welkom bij de Saamdagen app!</Text>
+              <Text lightColor='#fff'>
                 Voor je kan beginnen, gaan we eerst samen de app instellen.
               </Text>
               <NextPageButton
@@ -100,7 +100,7 @@ const OnboardingScreen = () => {
                 alignItems: 'center',
               }}
             >
-              <Text>
+              <Text lightColor='#fff'>
                 Mogen we jou meldingen sturen?{'\n'}
                 Zo kunnen we je sneller op de hoogte brengen als er iets te doen
                 is.
@@ -135,7 +135,7 @@ const OnboardingScreen = () => {
                 alignItems: 'center',
               }}
             >
-              <Text>Wat wil je nu doen?</Text>
+              <Text lightColor='#fff'>Wat wil je nu doen?</Text>
 
               <View
                 style={{
